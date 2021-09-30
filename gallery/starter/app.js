5de97e02a6b2edae9bdf166e18c2f57fb114efa5
jpeg25 <jairo25509@hotmail.com>
2021-09-29 23:20:23 -0500

    Gallery HTML ready.

--- gallery/starter/app.js
+++ gallery/starter/app.js
@@ -7,3 +7,17 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(HTMLelement) {
+  this.pictures = HTMLelement.querySelectorAll("img");
+
+  this.modal = getElement(".modal");
+  this.modalImg = getElement(".main-img");
+  this.modalImagesContainer = getElement(".modal-images");
+  this.closeBtn = getElement(".close-btn");
+  this.prevBtn = getElement(".prev-btn");
+  this.nextBtn = getElement(".next-btn");
+}
+
+const nature = new Gallery(getElement(".nature"));
+const city = new Gallery(getElement(".city"));
